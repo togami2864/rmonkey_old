510308e4df97f0b9094d40dbd0ed2ba1e9520765
togami2864 <tuabobo123@gmail.com>
2022-06-19 22:29:36 +0900

    feat: define token

---
+++ src/token.rs
@@ -0,0 +1,18 @@
+#[derive(Debug)]
+pub enum Token {
+    Illegal,
+    Eof,
+    Ident,
+    Int,
+    Assign,
+    Plus,
+    Comma,
+    Semicolon,
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+
+    Function,
+    Let,
+}
